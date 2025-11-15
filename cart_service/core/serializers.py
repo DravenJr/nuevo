ae5b8494f8f185e9ce320e0ebcb5940bd0d2b4a4
DravenJr <rivero2012x@hotmail.com>
2025-11-15 19:37:18 -0300

    fix: corrección en Dockerfile y ajustes en settings

--- cart_service/core/serializers.py
+++ cart_service/core/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
-from .models import Cart, CartItem
-from core.serializers import ProductSerializer
+from .models import CartItem
 
 class CartItemSerializer(serializers.ModelSerializer):
-    product = ProductSerializer(read_only=True)
-    product_id = serializers.PrimaryKeyRelatedField(
-        queryset=Product.objects.all(), write_only=True
-    )
-
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'product_id', 'quantity', 'unit_price']
+        fields = ['id', 'product_id', 'product_name', 'unit_price', 'quantity']
