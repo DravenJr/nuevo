7e251a239c589f10237ae1ff82cd80857372f5f1
DravenJr <rivero2012x@hotmail.com>
2025-11-15 19:02:21 -0300

    fix: corrección en Dockerfile y ajustes en settings

--- gateway/core/views.py
+++ gateway/core/views.py
@@ -1,11 +1,9 @@
+import requests
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def index(request):
     return render(request, "index.html")
-
-import requests
-from django.http import HttpResponse, JsonResponse
 
 # Mapeo de servicios internos
 CONTAINERS = {
