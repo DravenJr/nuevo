367e20628130b470a856d5ac25f3c4e3335e08ee
DravenJr <rivero2012x@hotmail.com>
2025-11-17 14:04:06 -0500

    nuevo

--- auth_service/core/rabbitmq.py
+++ auth_service/core/rabbitmq.py
@@ -2,27 +2,26 @@ import pika
 import os
 import json
 
-RABBITMQ_URL = os.environ.get("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
+RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
 
 def send_user_created_message(user):
-    """
-    Envía un mensaje a RabbitMQ cuando se crea un usuario.
-    user: instancia del modelo User
-    """
-    connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
-    channel = connection.channel()
-    channel.queue_declare(queue="users_queue", durable=True)
-    
-    message = {
-        "id": user.id,
-        "username": user.username,
-        "email": user.email
-    }
+    try:
+        connection = pika.BlockingConnection(pika.URLParameters(RABBITMQ_URL))
+        channel = connection.channel()
+        channel.queue_declare(queue="users_queue", durable=True)
+        
+        message = {
+            "id": user.id,
+            "username": user.username,
+            "email": user.email
+        }
 
-    channel.basic_publish(
-        exchange='',
-        routing_key='users_queue',
-        body=json.dumps(message),
-        properties=pika.BasicProperties(delivery_mode=2)
-    )
-    connection.close()
+        channel.basic_publish(
+            exchange='',
+            routing_key='users_queue',
+            body=json.dumps(message),
+            properties=pika.BasicProperties(delivery_mode=2)
+        )
+        connection.close()
+    except Exception as e:
+        print("No se pudo enviar el mensaje a RabbitMQ:", e)
